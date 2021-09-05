46ff7bc723ea53cc82da90e78847e313c0c816b5
Niji <mathereall@gmail.com>
2021-09-04 22:51:21 -0500

    espacio3d

--- campusdigital/src/pages/Index.vue
+++ campusdigital/src/pages/Index.vue
@@ -23,7 +23,7 @@
 
       </q-card>
       -->
-      <div ref="webgl" class="webgl"></div>
+      <Espacio3D/>
 
       <h4> {{ paginasPath.titulo }} </h4>
 
@@ -72,11 +72,12 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
 import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader.js'
+import Espacio3D from "components/Espacio3D";
 
 
 export default {
   name: 'PageIndex',
-  components: {},
+  components: {Espacio3D},
   data() {
     return {
       paginasPath: {contenido: []},
@@ -117,139 +118,12 @@ export default {
       return filtradas
     },
 
-    render() {
-      this.camera.position.x += (this.mouseX - this.camera.position.x) * .05;
-      this.camera.position.y += (-this.mouseY - this.camera.position.y) * .05;
-      this.camera.lookAt(this.scene.position);
-      this.renderer.render(this.scene, this.camera);
-    },
-
-    onDocumentMouseMove(event) {
-      this.mouseX = (event.clientX - this.windowHalfX) / 2;
-      this.mouseY = (event.clientY - this.windowHalfY) / 2;
-    },
-    onWindowResize() {
-      this.windowHalfX = window.innerWidth / 2;
-      this.windowHalfY = window.innerHeight / 2;
-      this.camera.aspect = window.innerWidth / window.innerHeight;
-      this.camera.updateProjectionMatrix();
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
-    },
-    loadModel() {
-      this.object.traverse((child) => {
-        if (child.isMesh) child.material.map = this.texture;
-      });
-      this.object.position.y = -95;
-      this.scene.add(this.object);
-    },
-    initThree() {
-
-      this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-
-      this.camera.position.z = 600;
-      this.camera.position.y = 2000;
-      this.camera.position.x = 2000;
-
-      // scene
-
-      this.scene = new THREE.Scene();
-
-
-      const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
-      this.scene.add(ambientLight);
-
-      const pointLight = new THREE.PointLight(0xffffff, 0.8);
-      this.camera.add(pointLight);
-      this.scene.add(this.camera);
-
-      // manager
-
-      function onError() {
-        console.log("onError")
-      }
-
-      const manager = new THREE.LoadingManager();
-      const loader = new OBJLoader(manager);
-      const textureLoader = new THREE.TextureLoader(manager);
-      this.texture = textureLoader.load('/textures/uv_grid_opengl.jpg');
-
-      loader.load('/models/edificio_cu_v1.obj', (obj) => {
-        this.edificio_cu = obj;
-      }, onProgress, onError);
-
-
-      manager.onLoad = () => {
-        console.log("on load")
-
-      }
-
-      loader.load('/models/teatro_jrr_v2.obj', (obj) => {
-        this.teatro_jrr = obj;
-      }, onProgress, onError);
-
-      manager.onLoad = () => {
-        console.log("on load")
-        this.edificio_cu.traverse((child) => {
-          if (child.isMesh) child.material.map = this.texture;
-        });
-        this.edificio_cu.position.y = -95;
-        this.scene.add(this.edificio_cu);
-        this.teatro_jrr.traverse((child) => {
-          if (child.isMesh) child.material.map = this.texture;
-        });
-        this.teatro_jrr.position.y = -95;
-        this.teatro_jrr.position.z = -95;
-        this.scene.add(this.teatro_jrr);
-      }
-
-
-      manager.onProgress = function (item, loaded, total) {
-
-        console.log(item, loaded, total);
-
-      };
-
-
-
-      // model
-
-      function onProgress(xhr) {
-
-        if (xhr.lengthComputable) {
-          const percentComplete = xhr.loaded / xhr.total * 100;
-          console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
-        }
-      }
-
-
-
-      //
-
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
-      this.$refs.webgl.appendChild(this.renderer.domElement);
-
-      document.addEventListener('mousemove', this.onDocumentMouseMove);
-
-      //
-
-      window.addEventListener('resize', this.onWindowResize);
-
-    },
-    animate() {
-      requestAnimationFrame(this.animate);
-      this.render();
-    },
   },
   watch: {
     '$route.path': function () {
       let path = this.$route.path.replace('/p/', '')
       path = path.replace('/', '')
       this.paginasPath = this.getPaginasPath(path)
-      this.camera.position.z = Math.random() * 1000
-      this.camera.position.y = Math.random() * 1000
-      this.camera.position.x = Math.random() * 1000
     },
     paginasIndex() {
       let path = this.$route.path.replace('/p/', '')
@@ -257,12 +131,5 @@ export default {
       this.paginasPath = this.getPaginasPath(path)
     }
   },
-  created() {
-    this.container = this.$refs.webgl;
-  },
-  mounted() {
-    this.initThree()
-    this.animate()
-  }
 }
 </script>
