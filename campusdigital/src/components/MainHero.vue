d9b964a649c1fd74db83d8e8268d1405b39f811d
lm <luismguzmanz@gmail.com>
2021-09-08 16:53:51 -0500

    Mas ConocerMas Dialog Dummy

--- campusdigital/src/components/MainHero.vue
+++ campusdigital/src/components/MainHero.vue
@@ -50,7 +50,7 @@
         <div class="row">
           <div class="col">
 
-            <main class="q-pb-xl">
+            <main>
               <div class="q-pa-md row items-start q-gutter-md row wrap justify-start q-mx-auto" style="max-width: 65rem;">
 
                 <div class="col-shrink col-xs-12 col-sm-8 self-center q-px-lg q-py-sm">
@@ -60,51 +60,78 @@
                     <h6 class="q-my-md text-primary text-uppercase">Universidad Michoacana <br>de San Nicolás de Hidalgo</h6>
                   </div>
                 </div>
-                <div class="col-shrink col-xs-12 col-sm-10 col-md-7 self-center q-px-lg q-py-sm">
+              </div>
+
+              <div class="q-pa-md row items-start q-gutter-md row wrap justify-start q-mx-auto" style="max-width: 65rem;">
+                <div class="order-lg-first col-shrink col-xs-12 col-sm-10 col-md-7 self-center q-px-lg q-py-sm">
                   <h5 class="q-mb-lg q-mt-sm text-blue-7 text-weight-light text-uppercase">¿Qué es el Campus Digital?</h5>
 
                   <p class="text-h6 text-blue-grey-9 text-weight-light">
                     Es un espacio dedicado a toda la comunidad Nicolaita, donde pueden encontrarse recursos, contenidos y servicios digitales en las áreas académicas, de investigación, difusión cultural y extensión universitaria, con recursos electrónicos como revistas, tesis, recorridos virtuales culturales, exposiciones, entre otros.
                   </p>
                 </div>
-
-                <q-card class="my-card self-stretch" flat bordered>
-                  <q-img
-                    src="https://firebasestorage.googleapis.com/v0/b/campus-b4087.appspot.com/o/Temp%2Fday18-floppy.png?alt=media&token=259a49b8-191a-4f33-873d-d4ef99d199ce"
-                  />
-                  <q-card-section>
-                    <div class="text-h5 q-mt-sm q-mb-xs">Adipisci eligendi</div>
-                    <div class="text-uppercase text-small text-orange-9">Explicabo inventore</div>
-                    <div class="text-caption text-grey q-my-md">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci eligendi est explicabo inventore, ipsa laboriosam laudantium quae quibusdam reiciendis sint temporibus vero. Assumenda cum dignissimos illo incidunt laboriosam nemo recusandae?
-                    </div>
-                  </q-card-section>
-
-                  <q-card-actions>
-                    <q-btn size="md" color="light" label="Enlace" class="text-white bg-primary" />
-
-                    <q-space />
-
-                    <q-btn
-                      color="grey"
-                      round
-                      flat
-                      dense
-                      :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-                      @click="expanded = !expanded"
-                    />
-                  </q-card-actions>
-
-                  <q-slide-transition>
-                    <div v-show="expanded">
-                      <q-separator />
-                      <q-card-section class="text-subitle2">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet autem commodi consectetur consequatur culpa cum, cupiditate ea enim error est fugit laboriosam nemo nihil omnis sed ullam, ut velit!
-                      </q-card-section>
-                    </div>
-                  </q-slide-transition>
-                </q-card>
+                <div class="order-lg-last col-shrink col-xs-12 col-sm-10 col-md-4 self-center q-px-lg q-py-sm">
+                  <q-img class="ripple" src="https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=654&q=80"></q-img>
+                </div>
               </div>
+
+              <div class="q-pa-md row items-start q-gutter-md row wrap justify-start q-mx-auto" style="max-width: 65rem;">
+                <div class="order-md-last col-shrink col-xs-12 col-sm-10 col-md-4 self-center q-px-lg q-py-sm">
+                  <q-img class="ripple" src="https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=654&q=80"></q-img>
+                </div>
+                <div class="order-md-first col-shrink col-xs-12 col-sm-10 col-md-7 self-center q-px-lg q-py-sm">
+                  <h5 class="q-mb-lg q-mt-sm text-blue-7 text-weight-light text-uppercase">¿Para qué es el Campus Digital?</h5>
+
+                  <p class="text-h6 text-blue-grey-9 text-weight-light">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam aspernatur atque doloribus enim, iure labore nisi porro quasi sed tempore, veritatis? Amet at consequatur consequuntur deleniti natus odio, placeat.
+                  </p>
+                </div>
+              </div>
+
+              <div class="full-width q-my-xl">
+                <div class="col-xs-12">
+                  <q-img class="ripple" src="https://firebasestorage.googleapis.com/v0/b/campus-b4087.appspot.com/o/Temp%2Fdrone_tmp.jpg?alt=media&token=d248d06e-31d7-4b1e-af3c-05f0290c079c"></q-img>
+                </div>
+              </div>
+
+
+              <div class="q-pa-md row items-start q-gutter-md row wrap justify-start q-mx-auto" style="max-width: 65rem;">
+                <div class="order-md-last col-shrink col-xs-12 col-sm-10 col-md-4 self-center q-px-lg q-py-sm">
+                  <q-img class="ripple" src="https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=654&q=80"></q-img>
+                </div>
+                <div class="order-md-first col-shrink col-xs-12 col-sm-10 col-md-7 self-center q-px-lg q-py-sm">
+                  <h5 class="q-mb-lg q-mt-sm text-blue-7 text-weight-light text-uppercase">¿Como es el Campus Digital?</h5>
+
+                  <p class="text-h6 text-blue-grey-9 text-weight-light">
+                    Beatae error eveniet hic recusandae! Ab aut, dolore odit porro quae recusandae rem rerum sed unde. Accusamus beatae impedit quibusdam quisquam suscipit.
+                  </p>
+                </div>
+              </div>
+
+              <div class="full-width">
+                <div class="q-mt-xl bg-primary q-py-xl q-pa-md row items-start q-gutter-md row wrap justify-center">
+                  <div class="col-shrink col-xs-12 col-sm-10 col-md-7 self-center q-px-lg q-py-sm">
+                    <p class="text-h6 text-white text-weight-light">
+                      Debitis explicabo ipsum officia!
+                    </p>
+                    <p class="text-body2 text-blue-4">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias est in numquam officiis quidem, unde? Accusamus cum eaque expedita iusto similique unde voluptatem? Deserunt dolore iure quisquam sed totam?
+                    </p>
+                  </div>
+                  <div class="col-shrink col-xs-12 col-sm-10 col-md-7 self-center q-px-lg q-py-sm">
+                    <q-btn size="sm"
+                           v-show="!showRightDrawer"
+                           @click="showRightDrawer=true"
+                           unelevated
+                           no-wrap
+                           class="q-pa-xs bg-white text-primary no-wrap">
+                      <span class="text-body2">Explorar</span>
+                      <q-icon name="las la-arrow-right" size="md" class="q-pl-md"/>
+                    </q-btn>
+                  </div>
+                </div>
+              </div>
+
             </main>
 
           </div>
