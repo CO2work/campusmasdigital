d40e9ec5128ae15d5ff34ac191c63078b08c4be7
Niji <mathereall@gmail.com>
2021-09-08 11:49:11 -0500

    esconder boton explorar si se muestra el menú

--- campusdigital/src/components/MainHero.vue
+++ campusdigital/src/components/MainHero.vue
@@ -1,40 +1,67 @@
 <template>
   <div style="height: 100vh;">
-    <div class="full-height row wrap justify-center items-center content-center q-mx-auto"
+    <div class="full-height hide-scrollbar row wrap justify-center items-center content-center q-mx-auto"
          style="max-width: 65rem; position: relative">
-      <div class="col-shrink col-xs-12 col-sm-8 col-md-5 self-center q-ma-lg">
-        <h4 class="q-mb-lg text-primary text-weight-light">Campus Digital UMSNH</h4>
-        <p class="text-body2">
-          Conoce los espacios digitales de la Universidad, sus servicios digitales y navega el campus virtual
-        </p>
-        <q-btn size="sm"
-               unelevated
-               no-wrap
-               class="q-my-md q-mr-md q-pa-xs bg-primary text-white">
-          <span class="text-body2">Conocer más</span>
-          <q-icon name="las la-arrow-down" size="md" class="q-pl-md"/>
-        </q-btn>
 
-        <q-btn size="sm"
-               v-show="!showRightDrawer"
-               @click="showRightDrawer=true"
-               unelevated
-               no-wrap
-               class="q-pa-xs bg-white text-primary no-wrap">
-          <span class="text-body2">Explorar</span>
-          <q-icon name="las la-arrow-right" size="md" class="q-pl-md"/>
-        </q-btn>
-      </div>
+          <div class="col-shrink col-xs-12 col-sm-8 col-md-5 self-center q-ma-lg">
+            <h4 class="q-mb-lg text-primary text-weight-light">Campus Digital UMSNH</h4>
+            <p class="text-body2">
+              Conoce los espacios digitales de la Universidad, sus servicios digitales y navega el campus virtual
+            </p>
+            <q-btn size="sm"
+                   unelevated
+                   no-wrap
+                   @click="showConocerMas=true"
+                   class="q-my-md q-mr-md q-pa-xs bg-primary text-white">
+              <span class="text-body2">Conocer más</span>
+              <q-icon name="las la-arrow-down" size="md" class="q-pl-md"/>
+            </q-btn>
 
-      <div class="col-shrink col-xs-12 col-sm-8 col-md-5 self-center q-ma-lg">
-        <!--        <q-img src="~assets/img/logo-geometry-v2.svg"></q-img>-->
-      </div>
+            <q-btn size="sm"
+                   v-show="!showRightDrawer"
+                   @click="showRightDrawer=true"
+                   unelevated
+                   no-wrap
+                   class="q-pa-xs bg-white text-primary no-wrap">
+              <span class="text-body2">Explorar</span>
+              <q-icon name="las la-arrow-right" size="md" class="q-pl-md"/>
+            </q-btn>
+          </div>
+
+          <div class="col-shrink col-xs-12 col-sm-8 col-md-5 self-center q-ma-lg">
+            <!--        <q-img src="~assets/img/logo-geometry-v2.svg"></q-img>-->
+          </div>
     </div>
+
+
+    <q-dialog
+      v-model="showConocerMas"
+      :maximized="true"
+      transition-show="slide-up"
+      transition-hide="jump-down"
+      class="full-height"
+    >
+      <div class="column full-height full-width dot-grid bg-white">
+
+
+        <q-page-sticky position="top-left" class="z-top" :offset="[16, 16]">
+          <q-btn fab-mini color="primary" icon="las la-angle-left" @click="showConocerMas=false"/>
+        </q-page-sticky>
+
+        <div class="row">
+          <div class="col">
+
+
+          </div>
+        </div>
+      </div>
+    </q-dialog>
   </div>
 </template>
 
 <script>
 import {mapActions, mapGetters} from "vuex";
+import gsap from 'gsap'
 
 export default {
   name: 'MainHero',
@@ -62,11 +89,12 @@ export default {
     ...mapActions('campus', ['showRightDrawerSet']),
   },
   data() {
-    return {}
+    return {
+      showConocerMas: false
+    }
   },
 }
 </script>
-
 
 
 
